dcd893434fd1f660690b56a75ae5ca3ed5b0b768
Eric-Kadyrov <kadyrov_eric@yahoo.com>
2024-05-03 00:30:08 +0300

    Add files via upload

---
+++ numpy_script.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a 2x2 array of zeros
+arr = np.zeros((2, 2))
+
+print("Array of zeros:")
+print(arr)
+
+# Perform an operation
+arr += 4
+
+print("After adding 4 to each element:")
+print(arr)
