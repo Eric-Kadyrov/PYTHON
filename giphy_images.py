a7ca765896d3f73d47f39dd6767c9fc692e7450c
Eric-Kadyrov <kadyrov_eric@yahoo.com>
2024-05-07 00:17:14 +0300

    Add files via upload

---
+++ giphy_images.py
@@ -0,0 +1,22 @@
+import requests
+
+def search_gifs(query):
+    api_key = 'YOUR_GIPHY_API_KEY'  # Replace with your actual Giphy API key
+    url = 'https://api.giphy.com/v1/gifs/search'
+    params = {
+        'api_key': api_key,
+        'q': query,
+        'limit': 5,  # Number of results to return
+        'offset': 0,
+        'rating': 'G',
+        'lang': 'en'
+    }
+    response = requests.get(url, params=params)
+    gifs = response.json()
+    
+    for gif in gifs['data']:
+        print(gif['url'])
+
+if __name__ == '__main__':
+    query = input("Enter a search word for GIFs: ")
+    search_gifs(query)
